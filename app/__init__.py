78e9a886cc426a2b3700dd9f32ee8d5db38bd57a
ManuNunez <niuweme.nunez@gmail.com>
2025-11-29 13:33:05 -0600

    a

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from .routes.main import main_bp
 
-def crate_app():
+def create_app():
     app = Flask(__name__)
     
 
-    //registro de blueprints
 
     app.register_blueprint(main_bp)
 
