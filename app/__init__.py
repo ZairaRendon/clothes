dc5c5ab271f10acffae8839ae44d0515171d7abe
zaira <zaira.ser7@gmail.com>
2025-11-29 14:08:14 -0600

    models

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+from .models import Product, Customer, Supplier, Sale, ProductVariation
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object("app.config.Config")
