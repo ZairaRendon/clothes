d4dcef962fcaefce3a4cbac18dffb30d2cc5865e
zaira <zaira.ser7@gmail.com>
2025-11-29 14:29:05 -0600

    init

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
-from .routes.main import main_bp
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    
+    app.config.from_object("app.config.Config")
+    db.init_app(app)
 
+    from .models import Product, ProductVariation, Customer, Supplier, Sale, Discount
 
-    app.register_blueprint(main_bp)
-
+    with app.app_context():
+        db.create_all()
 
     return app
